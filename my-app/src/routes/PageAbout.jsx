6dae1928e1fee91576f902f1b702a6cb11a8ed97
Luana Bastos <lu.bastos13@gmail.com>
2025-11-16 22:18:07 -0800

    last changes

--- my-app/src/routes/PageAbout.jsx
+++ my-app/src/routes/PageAbout.jsx
@@ -13,6 +13,28 @@ function PageAbout() {
             </div>
           </div>
         </div>
+
+        <article>
+          <p>
+            Hi! I'm a creator with a background in architecture, graphic design,
+            and photography. After over 5 years working in the architecture
+            industry, I decided to follow a new path—one that combines logic,
+            creativity, and constant learning: web development. Today, I'm
+            focused on building clean, functional, and visually engaging
+            websites. My multidisciplinary experience helps me bring a unique
+            perspective to every project, from design to development. Outside of
+            the screen, I'm all about movement and energy. I’m passionate about
+            an active lifestyle: surfing (trying, at least), skating, and
+            running keep me inspired. And whether it’s a sunrise coffee or a
+            late-night coding session, you’ll probably find me with an energy
+            drink in hand.
+          </p>
+          <div>
+            <p>I’ve also got a pretty eclectic taste in</p>
+            <button>Music</button>
+            <p>Click here to check what I'm listening now</p>
+          </div>
+        </article>
       </section>
     </main>
   );
--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // Isso permite conexões externas via IP
-    port: 5174, // Opcional, mas bom para garantir a porta
-  }
+  // server: {
+  //   host: true, // Isso permite conexões externas via IP
+  //   port: 5174, // Opcional, mas bom para garantir a porta
+  // }
 })
 
