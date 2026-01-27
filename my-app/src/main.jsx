0383ea6f33b0e9fd68d02bdc13666df2191c87f7
Luana Bastos <lu.bastos13@gmail.com>
2026-01-27 18:01:08 -0300

    gsap things

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -2,16 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import AppRouter from "./routers/AppRouter";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
-import { HelmetProvider } from "react-helmet-async";
+
 
 import "./styles/styles.scss";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HelmetProvider>
+    
       <ThemeProvider>
         <AppRouter />
       </ThemeProvider>
-    </HelmetProvider>
+
   </StrictMode>
 );
